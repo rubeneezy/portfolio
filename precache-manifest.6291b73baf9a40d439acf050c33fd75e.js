53a1d8516b563ef4d0618607b92c249306f2bbee
rubeneezy <44353326+rubeneezy@users.noreply.github.com>
2021-11-22 02:40:23 +0000

    Deploying to gh-pages from  @ a98cdd754f5591c193fbfdd3b473e52359e05fe7 🚀

---
+++ precache-manifest.6291b73baf9a40d439acf050c33fd75e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e22713a93d7dc84bb10b0b1b24c1517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20442a0dd53af0baf3b5",
+    "url": "/static/css/3.f7bd9d92.chunk.css"
+  },
+  {
+    "revision": "c4a9f7dde8645d954e8a",
+    "url": "/static/css/4.4b5113d6.chunk.css"
+  },
+  {
+    "revision": "e6644745d1614f6345b7",
+    "url": "/static/css/main.8ec78d54.chunk.css"
+  },
+  {
+    "revision": "93b39d66faa2e59e5f9d",
+    "url": "/static/js/2.dd3d55d3.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.dd3d55d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20442a0dd53af0baf3b5",
+    "url": "/static/js/3.6717dbd6.chunk.js"
+  },
+  {
+    "revision": "c4a9f7dde8645d954e8a",
+    "url": "/static/js/4.20b3dc7c.chunk.js"
+  },
+  {
+    "revision": "e6644745d1614f6345b7",
+    "url": "/static/js/main.f82f0bc0.chunk.js"
+  },
+  {
+    "revision": "ba5ad946f73d1dec3fb8",
+    "url": "/static/js/runtime-main.05fe8759.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "13535fa16e8e106e626d8ddc33f70af2",
+    "url": "/static/media/YoodliLogo.13535fa1.png"
+  },
+  {
+    "revision": "9237afb5c3e87e61821298755a3306f1",
+    "url": "/static/media/coambition.9237afb5.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "bffe5fe4a97263e5a9717cba7b6bc59e",
+    "url": "/static/media/danielsfund.bffe5fe4.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "9395d40f83ef89ad4a7c4dfb5b65e509",
+    "url": "/static/media/pomonalogo.9395d40f.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "ec5cc17f72b177446d365a0c4c8b0c3d",
+    "url": "/static/media/uber-logo.ec5cc17f.png"
+  }
+]);
